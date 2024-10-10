95a52168308fbcb5fcbba1e4fce56c4c156faf07
Isaac Milarsky <imilarsky@gmail.com>
2024-10-10 13:44:10 -0500

    add functions
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- entrypoint.py
+++ entrypoint.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env -S python3 -B
 
 import os
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
+def add_checkout_as_safe_directory():
+    try:
+        check_output(['git','config','--global','--add','safe.directory','/github/workspace'])
+    except CalledProcessError:
+        print("Warning: Could not configure github workspace as a safe directory!")
+
+
+def run_gource_and_store_log(log_file_name):
+    check_output(['gource','--output-custom-log',log_file_name])
 
 if __name__ == "__main__":
     print("Hello world!")
     print(os.getcwd())
-    print(check_output(['git','config','--global','--add','safe.directory','/github/workspace']))
-    print(check_output(['gource','--output-custom-log','gource-log.log']))
-    print(check_output(['gource','--help']))
+    fname = 'gource-log.log'
 
-    with open('gource-log.log','r') as f:
+    add_checkout_as_safe_directory()
+    run_gource_and_store_log(fname)
+
+    with open(fname,'r') as f:
         print(f.read())
