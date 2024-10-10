38c0abe78bf177dba066ddbde6989f6ee9ea51ae
Isaac Milarsky <imilarsky@gmail.com>
2024-10-10 13:31:29 -0500

    test
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- entrypoint.py
+++ entrypoint.py
@@ -6,7 +6,8 @@ from subprocess import check_output
 
 if __name__ == "__main__":
     print("Hello world!")
-    print(check_output(['gource','--output-custom-log','gource-log.log']))
+    print(os.getcwd())
+    print(check_output(['ls','-larth']))
 
     with open('gource-log.log','r') as f:
         print(f.read())
