df53426548b41f2864d56d726d873154850246b3
Isaac Milarsky <imilarsky@gmail.com>
2024-10-10 13:36:51 -0500

    test
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- entrypoint.py
+++ entrypoint.py
@@ -7,7 +7,7 @@ from subprocess import check_output
 if __name__ == "__main__":
     print("Hello world!")
     print(os.getcwd())
-    print(check_output(['git','config','--global','add','safe.directory','/github/workspace']))
+    print(check_output(['git','config','--global','--add','safe.directory','/github/workspace']))
     print(check_output(['gource','--output-custom-log','gource-log.log']))
     print(check_output(['gource','--help']))
 
